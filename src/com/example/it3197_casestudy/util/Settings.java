c5f326ac36abd1a2c9ce48c609cfe9a94e3334f0
= <joseph-50@live.com>
2014-06-25 16:22:49 +0800

    Community Outreach v0.5.1

--- src/com/example/it3197_casestudy/util/Settings.java
+++ src/com/example/it3197_casestudy/util/Settings.java
@@ -1,10 +1,16 @@
 package com.example.it3197_casestudy.util;
 
+import java.text.SimpleDateFormat;
+
 /**
  * This interface is to make coding easier
  * @author Lee Zhuo Xun
  */
 public interface Settings {
 	String DROPBOX_API_KEY = "cqvf3nim3klslqb";
-	
+	//String API_URL = "http://192.168.1.185/CommunityOutreach/a/";
+	String API_URL = "http://192.168.43.8/CommunityOutreach/a/";
+	SimpleDateFormat dateFormatter = new SimpleDateFormat("dd MMM yyyy");
+	SimpleDateFormat timeFormatter = new SimpleDateFormat("HH:mm a");
+	SimpleDateFormat dateTimeFormatter = new SimpleDateFormat("d MMM yyyy h:mm a");
 }
