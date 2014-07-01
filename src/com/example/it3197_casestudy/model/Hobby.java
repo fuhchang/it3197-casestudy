92b04b2b973cc27a9ef89ed26dbc3e5b120c2a23
fuhchang <fuhchang123@gmail.com>
2014-07-01 09:10:45 +0800

    sqlite for case study

---
+++ src/com/example/it3197_casestudy/model/Hobby.java
@@ -0,0 +1,58 @@
+package com.example.it3197_casestudy.model;
+
+public class Hobby {
+	private int groupID;
+	private String groupName;
+	private String category;
+	private String location;
+	private String description;
+	private int active;
+
+	public int getGroupID() {
+		return groupID;
+	}
+
+	public void setGroupID(int groupID) {
+		this.groupID = groupID;
+	}
+
+	public String getGroupName() {
+		return groupName;
+	}
+
+	public void setGroupName(String groupName) {
+		this.groupName = groupName;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getActive() {
+		return active;
+	}
+
+	public void setActive(int active) {
+		this.active = active;
+	}
+}
