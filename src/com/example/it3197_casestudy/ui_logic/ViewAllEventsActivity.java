693f4c167b7ebeee80e85cc9cfbcda1fbad8acb6
= <joseph-50@live.com>
2014-07-09 23:14:13 +0800

    Minor changes

--- src/com/example/it3197_casestudy/ui_logic/ViewAllEventsActivity.java
+++ src/com/example/it3197_casestudy/ui_logic/ViewAllEventsActivity.java
@@ -5,8 +5,12 @@ import com.example.it3197_casestudy.controller.GetAllEvents;
 
 import android.os.Bundle;
 import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.FragmentManager;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Typeface;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.Menu;
@@ -15,6 +19,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class ViewAllEventsActivity extends FragmentActivity {
 	ListView lvViewAllEvents;
@@ -28,16 +33,6 @@ public class ViewAllEventsActivity extends FragmentActivity {
 		
 		GetAllEvents getAllEvents = new GetAllEvents(ViewAllEventsActivity.this,lvViewAllEvents);
 		getAllEvents.execute();
-		lvViewAllEvents.setOnItemClickListener(new OnItemClickListener() {
-			@Override
-			public void onItemClick(AdapterView<?> parent, View arg1, int position,long arg3) {
-				// TODO Auto-generated method stub
-		        final String item = (String) parent.getItemAtPosition(position);
-		        Log.i("Item: ",item);
-		        Intent i = new Intent(ViewAllEventsActivity.this,ViewEventsActivity.class);
-		        startActivity(i);
-			}
-		});
 	}
 
 	@Override
@@ -51,24 +46,32 @@ public class ViewAllEventsActivity extends FragmentActivity {
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
 			case R.id.create_event:
-					AlertDialog.Builder selectTypeOfEventDialog = new AlertDialog.Builder(ViewAllEventsActivity.this);
-					selectTypeOfEventDialog.setTitle("Select type of event");
-					selectTypeOfEventDialog.setItems(R.array.type_of_events, new DialogInterface.OnClickListener() {
-						@Override
-						public void onClick(DialogInterface dialog, int which) {
-							// TODO Auto-generated method stub
-							String[] typeOfEvents = getResources().getStringArray(R.array.type_of_events);
-							String choice = typeOfEvents[which];
-							Intent intent = new Intent(ViewAllEventsActivity.this, CreateEventStep1Activity.class);
-			    			intent.putExtra("typeOfEvent", choice);
-			    			startActivity(intent);
-			    			ViewAllEventsActivity.this.finish();
-						}
-					});
-					selectTypeOfEventDialog.show();
-					break;
+				SelectTypeOfEventDialog selectTypeOfEventDialog = new SelectTypeOfEventDialog();
+				selectTypeOfEventDialog.show(this.getSupportFragmentManager(),"EventTypeDialog");
+				break;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+	
+	public static class SelectTypeOfEventDialog extends DialogFragment{
+		public Dialog onCreateDialog(Bundle savedInstanceState){
+			AlertDialog.Builder selectTypeOfEventDialog = new AlertDialog.Builder(getActivity());
+			selectTypeOfEventDialog.setTitle("Select type of event");
+			
+			selectTypeOfEventDialog.setItems(R.array.type_of_events, new DialogInterface.OnClickListener() {
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					// TODO Auto-generated method stub
+					String[] typeOfEvents = getResources().getStringArray(R.array.type_of_events);
+					String choice = typeOfEvents[which];
+					Intent intent = new Intent(getActivity(), CreateEventStep1Activity.class);
+	    			intent.putExtra("typeOfEvent", choice);
+	    			startActivity(intent);
+	    			getActivity().finish();
+				}
+			});
+			return selectTypeOfEventDialog.create();
+		}
 	}
 
 }
