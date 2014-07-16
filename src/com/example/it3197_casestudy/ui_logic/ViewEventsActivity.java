4b50e86f3edc16ea3f5334cf0073345a8e86894b
= <joseph-50@live.com>
2014-07-16 23:13:05 +0800

    Fix error

--- src/com/example/it3197_casestudy/ui_logic/ViewEventsActivity.java
+++ src/com/example/it3197_casestudy/ui_logic/ViewEventsActivity.java
@@ -77,8 +77,8 @@ public class ViewEventsActivity extends FragmentActivity implements ActionBar.Ta
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		/*getMenuInflater().inflate(R.menu.view_events, menu);
-		return true;*/
+		//getMenuInflater().inflate(R.menu.view_events, menu);
+		return false;
 	}
 
 	@Override
