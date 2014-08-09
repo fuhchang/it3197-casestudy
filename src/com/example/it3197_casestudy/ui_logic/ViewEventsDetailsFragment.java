31377398a922107a6c11eac2a3a5a01582285480
= <joseph-50@live.com>
2014-08-09 22:20:43 +0800

    Pass intent done for request hobby

--- src/com/example/it3197_casestudy/ui_logic/ViewEventsDetailsFragment.java
+++ src/com/example/it3197_casestudy/ui_logic/ViewEventsDetailsFragment.java
@@ -336,7 +336,6 @@ public class ViewEventsDetailsFragment extends Fragment implements Settings{
 			}
 			break;
 		case R.id.view_request:
-
 			Intent intentRequest = new Intent(this.getActivity(), RequestForHobbyHelp.class);
 			intentRequest.putExtra("eventID", event.getEventID());
 			startActivity(intentRequest);
